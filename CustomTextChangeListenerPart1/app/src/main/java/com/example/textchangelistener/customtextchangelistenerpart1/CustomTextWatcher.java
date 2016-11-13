7490a335acfed92d19c917663e19b1f6dfbd61ad
Sachin Sarawgi <sachinsarawgi201143@gmail.com>
2016-11-13 19:32:44 +0530

    CustomTextChangeListener for EditText in Android

---
+++ CustomTextChangeListenerPart1/app/src/main/java/com/example/textchangelistener/customtextchangelistenerpart1/CustomTextWatcher.java
@@ -0,0 +1,50 @@
+package com.example.textchangelistener.customtextchangelistenerpart1;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
+
+/**
+ * Created by SachinSarawgi on 13-11-2016.
+ */
+
+public class CustomTextWatcher implements TextWatcher {
+
+    private EditText editText;
+    private TextView textView;
+    private String previous = "";
+    private String actualText = "";
+
+    public CustomTextWatcher(EditText editText, TextView textView){
+
+        this.editText = editText;
+        this.textView = textView;
+    }
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        String temp = editText.getText().toString();
+        if(temp.length()>previous.length()){
+            if(temp.charAt(temp.length()-1)=='\n'){
+                actualText = actualText + "EN,";
+            }else{
+                actualText = actualText + temp.charAt(temp.length()-1)+",";
+            }
+        }else{
+            actualText = actualText + "BS,";
+        }
+        previous = temp;
+        textView.setText(actualText);
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+
+    }
+}
---
+++ CustomTextChangeListenerPart1/app/src/main/java/com/example/textchangelistener/customtextchangelistenerpart1/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.textchangelistener.customtextchangelistenerpart1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editText;
+    private TextView textView;
+    private String previous = "";
+    private String actualText = "";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = (EditText) findViewById(R.id.editText);
+        textView = (TextView) findViewById(R.id.textView);
+
+        editText.addTextChangedListener(new CustomTextWatcher(editText, textView));
+    }
+}
